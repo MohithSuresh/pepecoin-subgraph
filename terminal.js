985efef7996502064f724e646bface9cd0b5eff8
MohithSuresh <mohithsuresh77@gmail.com>
2023-06-03 12:31:01 +0530

    built and deployed

---
+++ terminal.js
@@ -0,0 +1,8 @@
+// ✔ Upload subgraph to IPFS
+
+// Build completed: QmW9hxjVfhDp6q89L2Pq5fjNxL4rGA13sPYomQKtjxionZ
+
+// Deployed to https://thegraph.com/studio/subgraph/pepecoin-subgraph
+
+// Subgraph endpoints:
+// Queries (HTTP):     https://api.studio.thegraph.com/query/47861/pepecoin-subgraph/y
